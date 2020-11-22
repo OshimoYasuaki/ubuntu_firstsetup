0db3bb2d137022c5bddaf4a3129b7c3e36feee73
OshimoYasuaki <58316987+OshimoYasuaki@users.noreply.github.com>
2020-11-23 04:20:37 +0900

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -2,20 +2,4 @@ echo "expoer LANG=en_US" >> ~/.bashrc
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get install network-manager -y
-echo "
-    wifis:
-        wlan0:
-            dhcp4: true
-            dhcp6: true
-            addresses: [192.168.0.100/24]
-            gateway4: 192.168.0.0 #wifiルータのip
-            nameservers:
-                 addresses: [192.168.0.0]
-                 search: []
-            optional: true
-            access-points:
-               "wifi name":
-                   password: hogehoge
-            
-" >> /etc/netplan/50-cloud-init.yaml
 sudo vim /etc/netplan/50-cloud-init.yaml
